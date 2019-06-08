f7f97b1a75bbfbda245ca9eac769f02898c444e2
Dloft <derek.lofton1@gmail.com>
2019-06-08 11:07:01 -0400

    arrays

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
  const users = 
-{
-   one: {
+[
+  {
         name: "Derek",
-        head: "this",
         fact: "Collects Iron Man Merch."
     }, 
-    two: {
+    {
         name: "Frank",
-        head: src="https://tenor.com/sO2r.gif",
         fact: "Has a Razer scooter."
     }
     , 
-    three: {
+     {
         name: "Mary",
-        head: src="https://tenor.com/1W67.gif",
         fact: "Never clibed a ladder."
     }
-};
-// var users = ["Iron Man", "Thor", "Spider Man", "Scarlet Witch", "Hulk", "Vision", "Black Widow", "Hawk Eye"];
+];
+
 
 function getUser() {
-    console.log(users.one.fact);
-  $("#head").text(users.one.head);
-  $("#name").text(users.one.name);
-  $("#fact").text(users.one.fact);
+    const randomizer = JSON.stringify(users[Math.floor(Math.random() * users.length)]);
+
+    console.log(randomizer);
+    $("#name").text(randomizer);
+        // $("#name").text(users.one.name);
+        // $("#fact").text(users.one.fact);
 }
+
